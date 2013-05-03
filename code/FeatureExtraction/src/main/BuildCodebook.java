655a92c6263150b90e642536cd3c524785458525
asrata <nju.angli@gmail.com>
2013-05-03 14:43:42 -0400

    update kmeans renaming cluster ids

--- code/FeatureExtraction/src/main/BuildCodebook.java
+++ code/FeatureExtraction/src/main/BuildCodebook.java
@@ -18,19 +18,28 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.SequenceFile;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.mahout.clustering.Cluster;
 import org.apache.mahout.clustering.classify.WeightedVectorWritable;
 import org.apache.mahout.clustering.conversion.InputDriver;
+import org.apache.mahout.clustering.iterator.ClusterWritable;
 import org.apache.mahout.clustering.kmeans.KMeansDriver;
+import org.apache.mahout.clustering.kmeans.Kluster;
 import org.apache.mahout.clustering.kmeans.RandomSeedGenerator;
+import org.apache.mahout.common.HadoopUtil;
+import org.apache.mahout.common.Pair;
 import org.apache.mahout.common.distance.DistanceMeasure;
 import org.apache.mahout.common.distance.EuclideanDistanceMeasure;
+import org.apache.mahout.common.iterator.sequencefile.PathFilters;
+import org.apache.mahout.common.iterator.sequencefile.SequenceFileIterable;
 import org.apache.mahout.math.NamedVector;
 import org.apache.mahout.math.VectorWritable;
 import org.apache.mahout.utils.clustering.ClusterDumper;
@@ -73,13 +82,37 @@ public class BuildCodebook extends Configured implements Tool {
     writer.close();
   }
 
-  public static void run(Configuration conf, Path input, Path clustersIn, Path output,
-      DistanceMeasure measure, double convergenceDelta, int maxIterations, boolean runClustering,
-      double clusterClassificationThreshold, boolean runSequential) throws IOException,
-      InterruptedException, ClassNotFoundException {
-
-    // iterate until the clusters converge
-
+  @SuppressWarnings("deprecation")
+  public void RenameClusterIds(Configuration conf, Path input, Path output) throws IOException {
+    FileSystem fs = FileSystem.get(conf);
+    HadoopUtil.delete(conf, output);
+    Path inputPathPattern;
+    if (fs.getFileStatus(input).isDir())
+      inputPathPattern = new Path(input, "*");
+    else
+      inputPathPattern = input;
+    FileStatus[] inputFiles = fs.globStatus(inputPathPattern, PathFilters.logsCRCFilter());
+    int clusterId = 0;
+    IntWritable newKey = new IntWritable();
+    ClusterWritable newValue = new ClusterWritable();
+    for (FileStatus fileStatus : inputFiles) {
+      if (fileStatus.isDir())
+        continue;
+      Path outFile = new Path(output, fileStatus.getPath().getName());
+      SequenceFile.Writer writer = SequenceFile.createWriter(fs, conf, outFile, IntWritable.class,
+          ClusterWritable.class);
+      for (Pair<Writable, ClusterWritable> record : new SequenceFileIterable<Writable, ClusterWritable>(
+          fileStatus.getPath(), true, conf)) {
+        ClusterWritable value = record.getSecond();
+        Kluster cluster = (Kluster) value.getValue();
+        Kluster newCluster = new Kluster(cluster.getCenter(), ++clusterId, cluster.getMeasure());
+        
+        newKey.set(clusterId);
+        newValue.setValue(newCluster);
+        writer.append(newKey, newValue);
+      }
+      writer.close();
+    }
   }
 
   public void KMeansClustering(Configuration conf, String inputSequencePath,
@@ -109,12 +142,17 @@ public class BuildCodebook extends Configured implements Tool {
         output, measure.getClass().getName() });
     LOG.info("convergence: {} max Iterations: {} num Reduce Tasks: {} Input Vectors: {}",
         new Object[] { convergenceDelta, maxIterations, VectorWritable.class.getName() });
-    
+
     Path clustersOut = KMeansDriver.buildClusters(conf, input, clusters, output, measure,
         maxIterations, delta, false);
 
+    Path clustersFinal = new Path(output, "clusters-final");
+
+    LOG.info("Renaming cluster IDs");
+    RenameClusterIds(conf, clustersOut, clustersFinal);
+
     LOG.info("Clustering data");
-    KMeansDriver.clusterData(conf, input, clustersOut, output, measure,
+    KMeansDriver.clusterData(conf, input, clustersFinal, output, measure,
         clusterClassificationThreshold, false);
 
   }
@@ -174,6 +212,7 @@ public class BuildCodebook extends Configured implements Tool {
   private static final String INPUT = "input";
   private static final String OUTPUT = "output";
   private static final String NUM_CLUSTERS = "K";
+  private static final String FUNC = "func";
 
   /**
    * Runs this tool.
@@ -188,6 +227,8 @@ public class BuildCodebook extends Configured implements Tool {
         .create(OUTPUT));
     options.addOption(OptionBuilder.withArgName("num").hasArg()
         .withDescription("number of clusters").create(NUM_CLUSTERS));
+    options.addOption(OptionBuilder.withArgName("path").hasArg().withDescription("tools")
+        .create(FUNC));
 
     CommandLine cmdline;
     CommandLineParser parser = new GnuParser();
@@ -212,13 +253,20 @@ public class BuildCodebook extends Configured implements Tool {
     String outputPath = cmdline.getOptionValue(OUTPUT);
     int numClusters = cmdline.hasOption(NUM_CLUSTERS) ? Integer.parseInt(cmdline
         .getOptionValue(NUM_CLUSTERS)) : 1024;
+    String func = cmdline.hasOption(FUNC) ? cmdline.getOptionValue(FUNC) : "all";
 
     LOG.info("Tool: " + BuildCodebook.class.getSimpleName());
 
     Configuration conf = getConf();
 
     // Kmeans using mahout
+    if (func.equals("all")) {
     KMeansClustering(conf, inputPath, outputPath, numClusters);
+    } else if (func.equals("rename")){
+      Path input = new Path(inputPath);
+      Path output = new Path(outputPath);
+      RenameClusterIds(conf, input, output);
+    }
 
     long startTime = System.currentTimeMillis();
     LOG.info("Job Finished in " + (System.currentTimeMillis() - startTime) / 1000.0 + " seconds");
