a24dbc7b857fc94e06c4a458499116f6b19eec54
Ubuntu <ubuntu@ip-10-8-51-30.ec2.internal>
2013-05-06 06:18:55 +0000

    update scripts

--- code/FeatureExtraction/prepare.sh
+++ code/FeatureExtraction/prepare.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-hadoop fs -ls /user/ubuntu/ang/data/MSRCv1/*s.bmp | awk 'BEGIN{cnt=0} $8!=""{++cnt;print cnt"\t"$8}' > MSRCv1.input
-
+hadoop fs -ls /user/ubuntu/ang/MSRCv1/data/*s.bmp | awk 'BEGIN{cnt=0} $8!=""{++cnt;print cnt"\t"$8}' > MSRCv1.input
+hadoop fs -put MSRCv1.input ang/MSRCv1/input.path
--- code/FeatureExtraction/run.sh
+++ code/FeatureExtraction/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
-#etc/hadoop-cluster.sh MrDenseSift -input ang/MSRCv1.input -output ang/feature/MSRCv1/raw -numReducers 15 -stepsize 8
-etc/hadoop-cluster.sh BuildCodebook -input ang/feature/MSRCv1/raw -output ang/feature/MSRCv1/codebook -K 200
+data=MSRCv1
+K=200
+etc/hadoop-cluster.sh MrDenseSift -input ang/$data/input.path -output ang/$data/feature -numReducers 16 -stepsize 8
+etc/hadoop-cluster.sh BuildCodebook -input ang/$data/feature -output ang/$data/codebook -K $K
